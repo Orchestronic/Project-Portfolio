b052155d385f6b617576a5d4daf011207956f301
Orchestronic <86896086+Orchestronic@users.noreply.github.com>
2021-11-24 15:38:31 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const panels = document.querySelectorAll('.panel');
+const background = document.querySelector('body');
+const activePan = document.querySelectorAll('.panel.active')
 
-panels.forEach(panel => {
-  panel.addEventListener('click', () => {
-   removeActiveClasses()
-   panel.classList.add('active');
-  })
-})
-
+ panels.forEach(panel => {
+   panel.addEventListener('click', () => {
+     removeActiveClasses();
+     panel.classList.add('active');
+	   background.style.backgroundImage = panel.style.backgroundImage;
+		 
+   })
+ })
+	
 function removeActiveClasses() {
 	panels.forEach(panel => {
-		panel.classList.remove('active');
-	})
+  	panel.classList.remove('active');
+  })
 }
+
+
+
+
+
 
