6713aa6e249c3308e395e5eb531d9cad614f24a9
Orchestronic <86896086+Orchestronic@users.noreply.github.com>
2021-12-07 17:20:57 -0800

    Create script.js

---
+++ Rotating Nav Menu/script.js
@@ -0,0 +1,11 @@
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const container = document.querySelector('.container');
+const closeTwo = document.getElementById('fix');
+
+open.addEventListener('click', () => container.classList.add('show-nav'));
+
+close.addEventListener('click', () => container.classList.remove('show-nav'));
+
+closeTwo.addEventListener('click', () => container.classList.remove('show-nav'));
+
